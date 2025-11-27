51e9ddbfed7b4ebfda7576dc9a0945997a3a849b
Sofa-art <lkdodik@yandex.ru>
2025-11-27 19:28:24 +0300

    Все исправления внесены, все тесты проходят

--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -7,17 +7,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+driver = webdriver.Firefox()
 
-@pytest.fixture(scope="module")
-def driver():
-    driver = webdriver.Firefox
-    driver.maximize_window()
-    yield driver
-    driver.quit()
-
-def test_form():
-    driver.get('https://www.saucedemo.com/')
+def test_form_validation():
+    driver.get("https://www.saucedemo.com/")
     Username = driver.find_element(By.CSS_SELECTOR,'#user-name')
     Username.send_keys("standard_user", Keys.RETURN)
     Password= driver.find_element(By.CSS_SELECTOR,'#password')
@@ -29,15 +22,15 @@ def test_form():
     tshirt_add_button = driver.find_element(By.XPATH, "//div[@class='inventory_item' and .//div[contains(text(), 'Sauce Labs Bolt T-Shirt')]]//button").click()
     onesie_add_button = driver.find_element(By.XPATH, "//div[@class='inventory_item' and .//div[contains(text(), 'Sauce Labs Onesie')]]//button").click()
     cart= driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
-    checkout = driver.find_element(By.ID, "checkout").click()
-    driver.find_element(By.NAME, "first-name").send_keys("Саният")
-    driver.find_element(By.NAME, "last-name").send_keys("Леонова")
-    driver.find_element(By.NAME, "postal-code").send_keys("187112")
+    checkout = driver.find_element(By.CSS_SELECTOR, "#checkout").click()
+    driver.find_element(By.CSS_SELECTOR, "#first-name").send_keys("Саният")
+    driver.find_element(By.CSS_SELECTOR, "#last-name").send_keys("Леонова")
+    driver.find_element(By.CSS_SELECTOR, "#postal-code").send_keys("187112")
     continue_button = driver.find_element(By.ID, "continue").click()
     total_cost = driver.find_element(By.CLASS_NAME, "summary_total_label").text
     total_cost_value = float(total_cost.split("$")[1])
     assert total_cost_value == 58.29
-    driver(quit)
+    driver.quit()
     
 
 
