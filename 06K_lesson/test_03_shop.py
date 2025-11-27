cec0152c663b1ca2ad7b4c556570af27ffca3234
Sofa-art <lkdodik@yandex.ru>
2025-11-27 16:37:03 +0300

    добавлено третье задание

--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -1,3 +1,4 @@
+import pytest
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
@@ -8,6 +9,13 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 from selenium.webdriver.common.keys import Keys
 
+@pytest.fixture(scope="module")
+def driver():
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
 def test_form():
     driver.get('https://www.saucedemo.com/')
     Username = driver.find_element(By.CSS_SELECTOR,'#user-name')
@@ -17,7 +25,20 @@ def test_form():
     button = driver.find_element(By.CSS_SELECTOR, "#login-button")
     button.click()
     waiter=WebDriverWait (driver, 50)
+    backpack_add_button = driver.find_element(By.XPATH, "//div[@class='inventory_item' and .//div[contains(text(), 'Sauce Labs Backpack')]]//button").click()
+    tshirt_add_button = driver.find_element(By.XPATH, "//div[@class='inventory_item' and .//div[contains(text(), 'Sauce Labs Bolt T-Shirt')]]//button").click()
+    onesie_add_button = driver.find_element(By.XPATH, "//div[@class='inventory_item' and .//div[contains(text(), 'Sauce Labs Onesie')]]//button").click()
+    cart= driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+    checkout = driver.find_element(By.ID, "checkout").click()
+    driver.find_element(By.NAME, "first-name").send_keys("Саният")
+    driver.find_element(By.NAME, "last-name").send_keys("Леонова")
+    driver.find_element(By.NAME, "postal-code").send_keys("187112")
+    continue_button = driver.find_element(By.ID, "continue").click()
+    total_cost = driver.find_element(By.CLASS_NAME, "summary_total_label").text
+    total_cost_value = float(total_cost.split("$")[1])
+    assert total_cost_value == 58.29
+    driver(quit)
+    
 
 
 
-driver.quit()
